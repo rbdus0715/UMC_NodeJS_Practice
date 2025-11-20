30c4230614ecd6313557ec24888f93edbf8531e0
rbdus0715 <rbdus0715@gmail.com>
2025-11-20 17:41:21 +0900

    Feat: swagger 설정

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import setupMiddleware from './middlewares/setup.middleware';
 import restaurantRouter from './routes/restaurant.route';
 import reviewRouter from './routes/review.route';
@@ -6,15 +6,35 @@ import missionRouter from './routes/mission.route';
 import userRouter from './routes/user.route';
 import userMissionRouter from './routes/userMission.route';
 import { errorHandler } from './middlewares/errorHandle.middleware';
-import compression from 'compression';
+import swaggerMiddleware from './middlewares/swagger.middleware';
+import swaggerAutogen from 'swagger-autogen';
 
 const app = express();
-app.use(
-  compression({
-    threshold: 512,
-  })
-);
+
 setupMiddleware(app);
+swaggerMiddleware(app);
+
+app.get(
+  '/openapi.json',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const options = {
+      openapi: '3.0.0',
+      disableLogs: true,
+      writeOutputFile: false,
+    };
+    const outputFile = '/dev/null';
+    const routes = ['../index.ts'];
+    const doc = {
+      info: {
+        title: 'UMC 9th',
+        description: 'UMC 9th Node.js 프로젝트입니다.',
+      },
+      host: 'localhost:3000',
+    };
+    const result = await swaggerAutogen(options)(outputFile, routes, doc);
+    res.json(result ? result.data : null);
+  }
+);
 
 app.use('/v1/restaurant', restaurantRouter);
 app.use('/v1/review', reviewRouter);
--- src/middlewares/setup.middleware.ts
+++ src/middlewares/setup.middleware.ts
@@ -3,8 +3,14 @@ import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
 
 const setupMiddleware = (app: express.Application) => {
+  app.use(
+    compression({
+      threshold: 512,
+    })
+  );
   app.use(cors());
   app.use(express.static('public'));
   app.use('/public', express.static(path.join(__dirname, 'public')));
